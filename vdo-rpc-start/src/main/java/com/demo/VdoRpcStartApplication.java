a841b30a9af925638577de03bf54edf6d70ebc35
shijie666 <chudonghai@126.com>
2020-12-16 19:55:47 +0800

    Initial commit

---
+++ vdo-rpc-start/src/main/java/com/demo/VdoRpcStartApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdoRpcStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdoRpcStartApplication.class, args);
+	}
+
+}
